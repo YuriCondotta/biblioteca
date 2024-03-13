3087aa87a3b1dd9aeca6ae7d72e787874f38b364
YuriCondotta <you@example.com>
2024-03-13 11:43:24 -0300

    criacao de novas classes

---
+++ src/Locacao.java
@@ -0,0 +1,76 @@
+import java.util.Date;
+
+public class Locacao {
+    private double valorLocacao;
+    private double valorConta;
+    private Funcionario funcionario;
+    private Usuario usuario;
+    private Livro livro;
+    private Date dataLocacao;
+    private Date dataDevolucao;
+    private Livro livroLocado;
+
+    public double getValorLocacao() {
+        return valorLocacao;
+    }
+
+    public void setValorLocacao(double valorLocacao) {
+        this.valorLocacao = valorLocacao;
+    }
+
+    public double getValorConta() {
+        return valorConta;
+    }
+
+    public void setValorConta(double valorConta) {
+        this.valorConta = valorConta;
+    }
+
+    public Funcionario getFuncionario() {
+        return funcionario;
+    }
+
+    public void setFuncionario(Funcionario funcionario) {
+        this.funcionario = funcionario;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Livro getLivro() {
+        return livro;
+    }
+
+    public void setLivro(Livro livro) {
+        this.livro = livro;
+    }
+
+    public Date getDataLocacao() {
+        return dataLocacao;
+    }
+
+    public void setDataLocacao(Date dataLocacao) {
+        this.dataLocacao = dataLocacao;
+    }
+
+    public Date getDataDevolucao() {
+        return dataDevolucao;
+    }
+
+    public void setDataDevolucao(Date dataDevolucao) {
+        this.dataDevolucao = dataDevolucao;
+    }
+
+    public Livro getLivroLocado() {
+        return livroLocado;
+    }
+
+    public void setLivroLocado(Livro livroLocado) {
+        this.livroLocado = livroLocado;
+    }
+}
